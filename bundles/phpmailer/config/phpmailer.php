adc41398b6c7d573c895a02eec165f7e09e95c66
Gagan Preet Singh <s.gagan.preet@gmail.com>
2012-07-24 18:53:33 +0530

    Trying newer things

---
+++ bundles/phpmailer/config/phpmailer.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Default Configuration
+ *
+ * For every option that is set here, if the value isn't null,
+ * then that setting will be passed to the constructor when
+ * starting a new mailer.
+ *
+ * For example, if the configuration contains:
+ *
+ *   array(
+ *     'SetFrom'   => array('colin@example.com', 'Colin Viebrock'),
+ *     'IsSMTP' => null,
+ *     'Host'   => 'smtp@example.com',
+ *   )
+ *
+ * that would be equivalent to running this when starting
+ * a new mailer instance:
+ *
+ *   $mailer->SetFrom('Colin@example.com', 'Colin Viebrock');
+ *   $mailer->IsSMTP();
+ *   $mailer->Host = 'smtp@example.com';
+ *
+ * Sadly, this is a bit limited, mostly because of how PHPMailer handles
+ * setting values.  Sometimes it's:
+ *
+ *    $foo->bar = $value
+ *
+ * and sometimes it's:
+ *
+ *    $foo->bar($value);
+ *
+ * Suggestions on how to better handle this are welcome, although the
+ * most common elements that you might want to prepopulate (from address,
+ * SMTP host and info) can be configured.
+ */
+
+return array(
+	'SetFrom'    => array('s.gagan.preet@gmail.com','Gagan Preet Singh'),
+	'IsSMTP'     => null,
+	'SMTPSecure' => 'tls',
+	
+	'Host'       => 'ssl://smtp.gmail.com',
+	'Port'       => '465',
+	'SMTPAuth'   => true,
+	'Username'   => 'noreply.theDeparted@gmail.com',
+	'Password'   => 'alphabetagamma',
+	'WordWrap'   => 10,
+);
