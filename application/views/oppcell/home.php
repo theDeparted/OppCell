82e353a10c2f5ae52e2ac07b8fbf26d4e123ad0c
Atul Singh Arora <toatularora@gmail.com>
2012-08-08 22:29:51 +0530

    Making the home page

--- application/views/oppcell/home.php
+++ application/views/oppcell/home.php
@@ -1,67 +1,23 @@
-<!-- <!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
 	<meta chrset="utf-8">
-	<title>Home</title>
-
+	<title>Opp Cell | Home</title>
 	<link rel="Shortcut Icon" href=<?php echo "\"".URL::base()."/img/favicon.ico\""; ?> >
-	<link rel="stylesheet" type="text/css" href=<?php echo "\"".URL::base()."/css/login.css\""; ?> />
-	<script type="text/javascript" src=<?php echo "\"".URL::base()."/js/jquery-1.7.2.js\""; ?>> </script>
+	<link rel="stylesheet" type="text/css" href=<?php echo "\"".URL::base()."/css/home.css\""; ?> />
 </head>
 
 <body>
-	<div id="la_verycenter"><div id="la_loadbar"></div><div id="la_status"></div>
-	<div id="la_inner"><div id="la_loading_text">Loading Resources</div></div></div>
-<div id="la_background_text"> 	
-	<center>
-	<img id="img_login" src=<?php echo "\"".URL::base()."/img/theDeparted_login_2.svg\""; ?> />
-	</center>
-</div>
-
-<div class="userinput_desc">
-	<center>
-	<!-- convert this bit to php, for a random function generating a new string everytime -->
-	<!-- <p class="userinput_caption">Your Name, Sapian!</p>--> -->
-<!-- 	<p id="feedback_title">Home Page</p>
-	<input id="main_input_box" class="userinput_text input_box" type="text"> </input>
-
-
-<div id="list_center_maker">
-<ul id="list_of_names">
-</ul>
-</div>
-
-<br/>
-<div class="userinput_desc">
-	<input id="password_input_box" class="userinput_text input_box" type="password" value="password" style="text-align:center;"> </input>
-	<br><br>
-	<p class="small_font"> <a id="change_user" class="links_below">change user name</a> | <a id="forgot_password" class="links_below">forgot password!</a> </p> </div>
-	<br><br>
-	<div id="msg_new_user" class="msg_below">
-		<p>Your password has been sent to your official IISER email account.</p>
-		<p>If you're unable to login, contact <a href="mailto:toatularora@gmail.com">theDeparted</a>.</p>
+	<div class="Bar">
+		<div class="Vertical_Align_Bottom Bar_height">
+			<div class="OppCell_logo">Opportunity Cell</div>
+		</div>
 	</div>
-	<div id="msg_no_guests" class="msg_below">
-		<p>Guest Login is currently unavailable. You will be redirected shortly.</p>
-		<p>Not a guest? Contact <a href="mailto:toatularora@gmail.com">theDeparted</a>.</p>
+	<div class="Bar_height">		
 	</div>
-	<div id="msg_forgot_password" class="msg_below">
-		<p>We've sent your password to your official IISER email account.</p>
-		<p>Did not receive an email? Contact <a href="mailto:toatularora@gmail.com">theDeparted</a>.</p>		
+	<div class="Options_Bar">
+		hello
 	</div>
-	<div id="msg_error" class="msg_below">		
-	</div>
-</div>
-</center>
-</div>
-<input style="display:none" type="text" id="lara_path" value=<?php echo "\"".URL::base()."\"" ?> > </input>
-<textarea style="display:none" rows="10" cols="100" id="DEBUG"><?php echo URL::base(); ?></textarea>
-
-<script type="text/javascript" src=<?php echo "\"".URL::base()."/js/login.js\""; ?>>	</script>
 </body>
 
-</html> -->
-<html>
-<h1> Now this is the home page!! </h1>
-<?php echo "\"".URL::base()."/img/theDeparted_login_2.svg\""; ?>
 </html>
