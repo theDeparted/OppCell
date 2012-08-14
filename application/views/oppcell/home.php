a5a6d5006eab246d3a5212f36918e0606abf3b75
Atul Singh Arora <toatularora@gmail.com>
2012-08-14 22:55:24 +0530

    Home Page towards completion

--- application/views/oppcell/home.php
+++ application/views/oppcell/home.php
@@ -22,7 +22,8 @@
 	<!-- <img src=<?php echo "\"".URL::base()."/img/default_m.jpg\""; ?> class="IMAGE" /> -->
 	<!-- <img src="http://www.fonemenu.com/blog/wp-content/uploads/2012/04/Sunset-In-Delhi.jpg" class="IMAGE" /> -->
 	<!-- <img src="http://farm5.staticflickr.com/4131/5002091188_9aa74aaa2a_o.jpg" class="IMAGE" /> -->
-	<img src="http://www.psdgraphics.com/wp-content/uploads/2010/11/hot-black-background.jpg" class="IMAGE" />
+	<!-- <img src="http://www.psdgraphics.com/wp-content/uploads/2010/11/hot-black-background.jpg" class="IMAGE" /> -->
+	<img src="https://github.com/theDeparted/OppCell/blob/master/resources/graphics/Home_Page/Home_Page.jpg?raw=true" class="IMAGE" />
 	<div class="Options_Bar">
 		<div class="Option1 option">RESEARCH <br/> @ <br/> IISER</div>
 		<div class="Option2 option">SUMMER <br/> PROJECTS</div>
