683921108aad9c4cb8b8e0ffc8155f99fc483ba0
Atul Singh Arora <toatularora@gmail.com>
2012-08-19 03:28:56 +0530

    Redirect fixed

--- application/views/oppcell/redirect.blade.php
+++ application/views/oppcell/redirect.blade.php
@@ -4,10 +4,11 @@
 		<link rel="Shortcut Icon" href=<?php echo "\"".URL::base()."/img/favicon.ico\""; ?> >
 		<script type="text/javascript" src=<?php echo "\"".URL::base()."/js/jquery-1.7.2.js\""; ?>> </script>
 	</head>
+	
 	<body>
 		Redirecting..
 		<script type="text/javascript"> 
-			var form = $('<form action="' + {{ URL::to('oppcell') }} + '" method="get">' +
+			var form = $('<form action="' + {{ "'".URL::to('oppcell')."'" }} + '" method="get">' + 
 			  '<input type="hidden" name="notBlank" value="Blank" />' +
 			  '</form>');
 			$('body').append(form);
