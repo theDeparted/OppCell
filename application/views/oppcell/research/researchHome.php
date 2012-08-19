dfdfe59dc3b3dfa2a3804e5a9d0049b430498ab5
Atul Singh Arora <toatularora@gmail.com>
2012-08-20 03:14:09 +0530

    ResearchHome

---
+++ application/views/oppcell/research/researchHome.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta chrset="utf-8">
+	<title>Research @ IISER</title>
+
+	<link rel="Shortcut Icon" href=<?php echo "\"".URL::base()."/img/favicon.ico\""; ?> >
+	<!-- <link rel="stylesheet" type="text/css" href=<?php echo "\"".URL::base()."/css/login.css\""; ?> /> -->
+	<script type="text/javascript" src=<?php echo "\"".URL::base()."/js/jquery-1.7.2.js\""; ?>> </script>
+</head>
+
+<body>
+	Research @ IISER
+</body>
+
+</html>
