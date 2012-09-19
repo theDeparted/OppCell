8361e8bd1d923b341c39607f7774956f7f3d21d7
Gagan Preet Singh <s.gagan.preet@gmail.com>
2012-09-19 21:43:37 +0530

    Faulty D

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -134,3 +134,66 @@
 			}
 		}
 
+		public function get_dlist()
+		{
+			$data = array();
+			$profs = Prof::all();
+			foreach($profs as $prof)
+			{
+				$dat = array(
+						'id' => $prof->id,
+						'name' => $prof->name,
+						'research_interest' => $prof->research_interest
+					);
+				$data[] = $dat;
+			}
+			return json_encode($data);
+		}
+
+		public function post_dadd()
+		{
+			$data = json_decode(Input::get('member'));
+			$mem = new Prof;
+			$mem->name = $data->name;
+			$mem->research_interest = $data->research_interest;
+			if($mem->save())
+			{
+				return "Member Adding Process ended Successfully";
+			}
+			else
+			{
+				return "Member Adding Process Failed";
+			}
+		}
+
+		public function post_dupdate()
+		{
+			$data = json_decode(Input::get('member'));
+			$mem = Prof::find($data->id);
+			$mem->name = $data->name;
+			$mem->research_interest = $data->research_interest;
+			if($mem->save())
+			{
+				return "Member Adding Process ended Successfully";
+			}
+			else
+			{
+				return "Member Adding Process Failed";
+			}
+		}
+
+		public function post_ddel()
+		{
+			$id = Input::get('id');
+			$mem = Prof::find($id);
+			if($mem->delete())
+			{
+				return "Member Remove Process ended Successfully";
+			}
+			else
+			{
+				return "Member Remove Process Failed";
+			}
+		}
+
+}
