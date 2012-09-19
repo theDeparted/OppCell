4d46721fc2e9729c1449da12c151796f20aa0332
Gagan Preet Singh <s.gagan.preet@gmail.com>
2012-09-19 07:58:08 +0530

    Removed Profs, Added Members and Journal

---
+++ application/migrations/2012_09_19_022038_del_profs_and_create_members.php
@@ -0,0 +1,65 @@
+<?php
+
+class Del_Profs_And_Create_Members {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::drop('profs');
+		Schema::create('members', function($table)
+		{
+			// Autoincrement ID
+			$table->increments('id')->unsigned();
+
+			// Name
+			$table->string('name');
+
+			// Image link
+			$table->string('img');
+
+			// Description
+			$table->string('research_interest');
+
+			// Title
+			$title->string('title');
+
+			// Timestamps
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('members');
+		Schema::create('profs', function($table)
+		{
+			// Autoincrement ID
+			$table->increments('id')->unsigned();
+
+			// Name
+			$table->string('name');
+
+			// Image link
+			$table->string('img');
+
+			// Description
+			$table->string('description');
+
+			// Title
+			$title->string('title');
+
+			// Timestamps
+			$table->timestamps();
+		});
+	}
+
+}
---
+++ application/migrations/2012_09_19_022305_add_journals.php
@@ -0,0 +1,49 @@
+<?php
+
+class Add_Journals {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('journals', function($table)
+		{
+			// Autoincrement ID
+			$table->increments('id')->unsigned();
+
+			// Name
+			$table->string('name');
+
+			// Image link
+			$table->string('rating');
+
+			// Description
+			$table->string('nationality');
+
+			// Timestamps
+			$table->timestamps();
+		});
+		Schema::table('papers', function($table)
+		{
+			$table->integer('Journal');
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('journals');
+		Schema::table('papers', function()
+		{
+			$table->drop_column('Journal');
+		});
+	}
+
+}
